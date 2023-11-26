59ab8c9dd1d7adb1c60acd04d2c000fe9a3607f8
returnaction <obergannv@gmail.com>
2023-11-26 00:57:19 -0500

    Add Post Get GetById Delete Put requests for Pizza

--- PizzaStore(MinimalAPI)/Program.cs
+++ PizzaStore(MinimalAPI)/Program.cs
@@ -35,6 +35,40 @@ namespace PizzaStore_MinimalAPI_
 
             app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
 
+            app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+
+            app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+            {
+                await db.Pizzas.AddAsync(pizza);
+                await db.SaveChangesAsync();
+                return Results.Created($"/pizza/{pizza.Id}", pizza);
+            });
+
+            app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+            {
+                Pizza pizza = await db.Pizzas.FindAsync(id);
+                if (pizza is null)
+                    return Results.NotFound();
+
+                pizza.Name = updatepizza.Name;
+                pizza.Description = updatepizza.Description;
+                await db.SaveChangesAsync();
+                return Results.NoContent();
+            });
+
+            app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+            {
+                var pizza = await db.Pizzas.FindAsync(id);
+                if (pizza is null)
+                {
+                    return Results.NotFound();
+                }
+                db.Pizzas.Remove(pizza);
+                await db.SaveChangesAsync();
+                return Results.Ok();
+            });
+
+
             app.Run();
         }
     }
